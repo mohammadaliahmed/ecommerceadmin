e7f51d3ff6d9e5f092f565afb21363206fa81b9e
ali ahmed <m.aliahmed0@gmail.com>
2018-03-13 22:00:34 +0500

    better

---
+++ app/src/main/java/com/appsinventiv/headphonesadmin/Activities/ProductList.java
@@ -0,0 +1,133 @@
+package com.appsinventiv.headphonesadmin.Activities;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.appsinventiv.headphonesadmin.Adapters.ProductsListAdapter;
+import com.appsinventiv.headphonesadmin.Models.ProductModel;
+import com.appsinventiv.headphonesadmin.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class ProductList extends AppCompatActivity {
+    RecyclerView recyclerView;
+    LinearLayoutManager layoutManager;
+    ArrayList<ProductModel> productList = new ArrayList<>();
+    DatabaseReference mDatabase;
+    ProductsListAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_list);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_product);
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new ProductsListAdapter(productList, ProductList.this, new ProductsListAdapter.onItemClick() {
+
+            @Override
+            public void onClick(View v,String productId, final boolean value) {
+                mDatabase.child("products").child(productId).child("isActive").setValue("" + value)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                if (value) {
+                                    Toast.makeText(ProductList.this, "Product is Active", Toast.LENGTH_SHORT).show();
+//                                    Intent i=new Intent(ProductList.this,ProductList.class);
+//                                    startActivity(i);
+//                                    finish();
+                                }else {
+                                    Toast.makeText(ProductList.this, "Product is Inactive" , Toast.LENGTH_SHORT).show();
+//                                    Intent i=new Intent(ProductList.this,ProductList.class);
+//                                    startActivity(i);
+//                                    finish();
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+            }
+        });
+        recyclerView.setAdapter(adapter);
+        loadCart();
+
+    }
+
+    private void loadCart() {
+        mDatabase.child("products").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if (dataSnapshot != null) {
+
+                    ProductModel model = dataSnapshot.getValue(ProductModel.class);
+                    if (model != null) {
+                        productList.add(model);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (item.getItemId() == android.R.id.home) {
+
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
---
+++ app/src/main/java/com/appsinventiv/headphonesadmin/Adapters/ProductsAdapter.java
@@ -0,0 +1,96 @@
+package com.appsinventiv.headphonesadmin.Adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.appsinventiv.headphonesadmin.Models.ProductModel;
+import com.appsinventiv.headphonesadmin.R;
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+/**
+ * Created by AliAh on 27/02/2018.
+ */
+
+public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.Viewholder> {
+    ArrayList<String> itemList;
+    Context context;
+    LayoutInflater layoutInflater;
+    DatabaseReference mDatabase;
+
+    public ProductsAdapter(ArrayList<String> itemList, Context context) {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        this.itemList = itemList;
+        this.context = context;
+        this.layoutInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public ProductsAdapter.Viewholder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.product_layout, parent, false);
+        ProductsAdapter.Viewholder viewHolder = new ProductsAdapter.Viewholder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final ProductsAdapter.Viewholder holder, int position) {
+        final String id = itemList.get(position);
+//        Toast.makeText(context, "" + id, Toast.LENGTH_SHORT).show();
+        mDatabase.child("products").child(id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot != null) {
+                    final ProductModel model = dataSnapshot.getValue(ProductModel.class);
+                    if (model != null) {
+
+                        holder.product_title.setText(model.getItemTitle());
+                        holder.product_price.setText("Rs " + model.getItemPrice());
+                        holder.product_sku.setText("SKU  "+model.getSku());
+
+
+                        Glide.with(context).load(model.getThumbnailUrl()).into(holder.product_image);
+
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return itemList.size();
+
+
+    }
+
+    public class Viewholder extends RecyclerView.ViewHolder {
+        public TextView product_title, product_sku, product_price;
+        public ImageView product_image;
+
+        public Viewholder(View itemView) {
+            super(itemView);
+            product_title = itemView.findViewById(R.id.product_title);
+            product_sku = itemView.findViewById(R.id.product_sku);
+            product_price = itemView.findViewById(R.id.product_price);
+            product_image = itemView.findViewById(R.id.product_image);
+        }
+    }
+}
