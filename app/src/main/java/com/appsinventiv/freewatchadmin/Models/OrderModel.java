1d2884f65cd9aa4d0f7a6ac657dee4514747928b
ali ahmed <m.aliahmed0@gmail.com>
2018-03-17 12:39:59 +0500

    free admin

---
+++ app/src/main/java/com/appsinventiv/freewatchadmin/Models/OrderModel.java
@@ -0,0 +1,127 @@
+package com.appsinventiv.freewatchadmin.Models;
+
+import java.util.ArrayList;
+
+/**
+ * Created by AliAh on 26/02/2018.
+ */
+
+public class OrderModel {
+    String orderId,userId,fullName,email,phone,address,city,orderStatus;
+    long time,totalPrice,itemCount;
+    ArrayList<String> productIdList;
+
+    public OrderModel() {
+    }
+
+    public OrderModel(String orderId, String userId, String fullName, String email, String phone, String address, String city, String orderStatus, long time, long totalPrice, long itemCount, ArrayList<String> productIdList) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.city = city;
+        this.orderStatus = orderStatus;
+        this.time = time;
+        this.totalPrice = totalPrice;
+        this.itemCount = itemCount;
+        this.productIdList = productIdList;
+    }
+
+    public ArrayList<String> getProductIdList() {
+        return productIdList;
+    }
+
+    public void setProductIdList(ArrayList<String> productIdList) {
+        this.productIdList = productIdList;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public long getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(long totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public long getItemCount() {
+        return itemCount;
+    }
+
+    public void setItemCount(long itemCount) {
+        this.itemCount = itemCount;
+    }
+}
---
+++ app/src/main/java/com/appsinventiv/freewatchadmin/Utils/MyFirebaseInstanceIDService.java
@@ -0,0 +1,39 @@
+package com.appsinventiv.freewatchadmin.Utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by AliAh on 01/03/2018.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    DatabaseReference mDatabase;
+    Context context = this;
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+        Log.d("refres",refreshedToken);
+        SharedPreferences pref = context.getSharedPreferences("userDetails", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("fcmKey", refreshedToken);
+        editor.apply();
+
+
+    }
+}
